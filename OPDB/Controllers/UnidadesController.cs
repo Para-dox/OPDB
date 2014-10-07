e5c38c3e635aca6b3b135fc5470c999a0f8dce66
Rolfeus <cpt4987@gmail.com>
2014-10-07 19:13:02 -0400

    my controlador

---
+++ OPDB/Controllers/UnidadesController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OPDB.Models;
+
+namespace OPDB.Controllers
+{
+    public class UnidadesController : Controller
+    {
+        private OPDBEntities db = new OPDBEntities();
+
+        //
+        // GET: /Unidades/
+
+        public ActionResult Index()
+        {
+            var units = db.Units.Include(u => u.User).Include(u => u.User1);
+            return View(units.ToList());
+        }
+
+        //
+        // GET: /Unidades/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Unit unit = db.Units.Find(id);
+            if (unit == null)
+            {
+                return HttpNotFound();
+            }
+            return View(unit);
+        }
+
+        //
+        // GET: /Unidades/Create
+
+        public ActionResult Create()
+        {
+            ViewBag.CreateUser = new SelectList(db.Users, "UserID", "UserPassword");
+            ViewBag.UpdateUser = new SelectList(db.Users, "UserID", "UserPassword");
+            return View();
+        }
+
+        //
+        // POST: /Unidades/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Unit unit)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Units.Add(unit);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CreateUser = new SelectList(db.Users, "UserID", "UserPassword", unit.CreateUser);
+            ViewBag.UpdateUser = new SelectList(db.Users, "UserID", "UserPassword", unit.UpdateUser);
+            return View(unit);
+        }
+
+        //
+        // GET: /Unidades/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Unit unit = db.Units.Find(id);
+            if (unit == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CreateUser = new SelectList(db.Users, "UserID", "UserPassword", unit.CreateUser);
+            ViewBag.UpdateUser = new SelectList(db.Users, "UserID", "UserPassword", unit.UpdateUser);
+            return View(unit);
+        }
+
+        //
+        // POST: /Unidades/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Unit unit)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(unit).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CreateUser = new SelectList(db.Users, "UserID", "UserPassword", unit.CreateUser);
+            ViewBag.UpdateUser = new SelectList(db.Users, "UserID", "UserPassword", unit.UpdateUser);
+            return View(unit);
+        }
+
+        //
+        // GET: /Unidades/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Unit unit = db.Units.Find(id);
+            if (unit == null)
+            {
+                return HttpNotFound();
+            }
+            return View(unit);
+        }
+
+        //
+        // POST: /Unidades/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Unit unit = db.Units.Find(id);
+            db.Units.Remove(unit);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
