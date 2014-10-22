327e67b35f666ed29807dad417003f039ee2e206
dabuddahn <mark.sukhram@upr.edu>
2014-10-22 12:13:18 -0400

    Changes to Alcance controller and associated views.

--- OPDB/Controllers/AlcanceController.cs
+++ OPDB/Controllers/AlcanceController.cs
@@ -27,17 +27,19 @@ namespace OPDB.Controllers
 
         public ActionResult Detalles(int id = 0)
         {
-            UserViewModel userViewModel = new UserViewModel
+            var outreach = db.OutreachEntityDetails.Find(id);
+
+            UserViewModel outreachViewModel = new UserViewModel
             {
-                outreachEntity = db.OutreachEntityDetails.Find(id),
-                Notes = from note in db.UserNotes.Include(note => note.NoteType) where note.UserID == id && note.DeletionDate == null select note
+                outreachEntity = outreach,
+                Notes = from note in db.UserNotes.Include(note => note.NoteType) where note.SubjectID == outreach.UserID && note.DeletionDate == null select note
             };
 
-            if (userViewModel.outreachEntity == null)
+            if (outreachViewModel.outreachEntity == null)
             {
                 return HttpNotFound();
             }
-            return View(userViewModel);
+            return View(outreachViewModel);
         }
 
         //
@@ -74,14 +76,17 @@ namespace OPDB.Controllers
 
         public ActionResult Editar(int id = 0)
         {
-            OutreachEntityDetail outreachentitydetail = db.OutreachEntityDetails.FirstOrDefault(i => i.UserID == id); 
-            if (outreachentitydetail == null)
+
+            UserViewModel outreachViewModel = new UserViewModel
+            {
+                outreachEntity = db.OutreachEntityDetails.Find(id)
+            };
+
+            if (outreachViewModel.outreachEntity == null)
             {
                 return HttpNotFound();
             }
-            ViewBag.OutreachEntityTypeID = new SelectList(db.OutreachEntityTypes, "OutreachEntityTypeID", "OureachEntityType", outreachentitydetail.OutreachEntityTypeID);
-            ViewBag.UserID = new SelectList(db.Users, "UserID", "UserPassword", outreachentitydetail.UserID);
-            return View(outreachentitydetail);
+            return View(outreachViewModel);
         }
 
         //
@@ -89,17 +94,19 @@ namespace OPDB.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Editar(OutreachEntityDetail outreachentitydetail)
+        public ActionResult Editar(UserViewModel outreachentitydetail)
         {
             if (ModelState.IsValid)
             {
-                outreachentitydetail.UpdateDate = DateTime.Now;
-                db.Entry(outreachentitydetail).State = EntityState.Modified;
+                outreachentitydetail.user.UpdateDate = DateTime.Now;
+                outreachentitydetail.outreachEntity.UpdateDate = DateTime.Now;
+                db.Entry(outreachentitydetail.user).State = EntityState.Modified;
+                db.Entry(outreachentitydetail.outreachEntity).State = EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
-            ViewBag.OutreachEntityTypeID = new SelectList(db.OutreachEntityTypes, "OutreachEntityTypeID", "OureachEntityType", outreachentitydetail.OutreachEntityTypeID);
-            ViewBag.UserID = new SelectList(db.Users, "UserID", "UserPassword", outreachentitydetail.UserID);
+            
+            outreachentitydetail.userTypes = getUserTypes();
             return View(outreachentitydetail);
         }
 
@@ -108,14 +115,174 @@ namespace OPDB.Controllers
 
         public ActionResult Remover(int id = 0)
         {
-            OutreachEntityDetail outreachentitydetail = db.OutreachEntityDetails.Find(id);
-            if (outreachentitydetail == null)
+            User outreach = db.Users.Find(id);
+            OutreachEntityDetail outreachDetail = db.OutreachEntityDetails.FirstOrDefault(i => i.UserID == id);
+            if (outreach == null)
             {
                 return HttpNotFound();
             }
-            return View(outreachentitydetail);
+            else
+            {
+                outreach.DeletionDate = DateTime.Now;
+                outreachDetail.DeletionDate = DateTime.Now;
+                db.Entry(outreach).State = EntityState.Modified;
+                db.Entry(outreachDetail).State = EntityState.Modified;
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
         }
-               
+
+        public ActionResult RemoverNota(int id)
+        {
+            var note = db.UserNotes.Find(id);
+            note.DeletionDate = DateTime.Now;
+            db.Entry(note).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Detalles", "Alcance", new { id = note.SubjectID });
+        }
+
+        [HttpPost]
+        public ActionResult CrearNota(int id)
+        {
+            EscuelasController controller = new EscuelasController();
+
+            UserViewModel outreachViewModel = new UserViewModel
+             {
+
+                 NoteTypes = controller.getNoteTypes(),
+                 note = new UserNote
+                 {
+
+                    SubjectID = id
+                 }
+             };
+
+             return PartialView("CrearNota", outreachViewModel);
+        }
+
+        [HttpPost]
+        public ActionResult GuardarNota(UserViewModel outreachViewModel)
+        {
+
+            outreachViewModel.note.UpdateUser = 2;
+            outreachViewModel.note.UpdateDate = DateTime.Now;
+
+            if (outreachViewModel.note.UserNoteID == 0)
+            {
+                if (ModelState.IsValid)
+                {
+                    outreachViewModel.note.CreateDate = DateTime.Now;
+
+                    outreachViewModel.note.UserID = 2;
+                    outreachViewModel.note.CreateUser = 2;
+
+                    db.UserNotes.Add(outreachViewModel.note);
+                    db.SaveChanges();
+
+                    return View("_Hack");
+                }
+
+                return Content(GetErrorsFromModelState(outreachViewModel));
+            }
+            else if (ModelState.IsValid)
+            {
+
+                db.Entry(outreachViewModel.note).State = EntityState.Modified;
+                db.SaveChanges();
+                return View("_Hack");
+
+            }
+
+            return Content(GetErrorsFromModelState(outreachViewModel));
+        }
+
+        public ActionResult EditarNota(int id)
+        {
+            EscuelasController controller = new EscuelasController();
+
+            UserViewModel outreachViewModel = new UserViewModel
+            {
+
+                NoteTypes = controller.getNoteTypes(),
+                note = db.UserNotes.Find(id)
+            };
+
+            return PartialView("EditarNota", outreachViewModel);
+        }
+
+        public ActionResult Lista()
+        {
+            var users = from o in db.OutreachEntityDetails.Include(o => o.OutreachEntityType) where o.DeletionDate == null select o;
+
+            return View(users.ToList());
+
+        }
+
+        public ActionResult Removidos()
+        {
+            var users = from o in db.OutreachEntityDetails.Include(o => o.OutreachEntityType) where o.DeletionDate != null select o;
+
+            return PartialView("Removidos", users.ToList());
+
+        }
+
+        public String GetErrorsFromModelState(UserViewModel outreachViewModel)
+        {
+
+
+            //retrieves the validation messages from the ModelState as strings    
+            var str = "";
+            var errorSates = from state in ModelState.Values
+                             from error in state.Errors
+                             select error.ErrorMessage;
+
+            var errorList = errorSates.ToList();
+            foreach (var m in errorList)
+            {
+                str = str + "<li>* " + m + "</li>";
+            }
+
+            return str;
+        }
+
+        public List<SelectListItem> getOutreachTypes()
+        {
+            List<SelectListItem> types = new List<SelectListItem>();
+            foreach (var outreachType in db.OutreachEntityTypes)
+            {
+
+                types.Add(new SelectListItem()
+                {
+                    Text = outreachType.OutreachEntityType1,
+                    Value = outreachType.OutreachEntityTypeID + ""
+
+                });
+
+            }
+
+            return types;
+        }
+
+        public List<SelectListItem> getUserTypes()
+        {
+            List<SelectListItem> types = new List<SelectListItem>();
+            foreach (var userType in db.UserTypes)
+            {
+
+                if (userType.UserTypeID > 2)
+                {
+                    types.Add(new SelectListItem()
+                    {
+                        Text = userType.UserType1,
+                        Value = userType.UserTypeID + ""
+
+                    });
+                }
+            }
+
+            return types;
+
+        }
 
         protected override void Dispose(bool disposing)
         {
